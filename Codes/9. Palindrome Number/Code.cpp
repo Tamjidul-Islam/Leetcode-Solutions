1cb82a066bd580d6ba7129d38269d662fab76b69
Tamjidul-Islam <tamjidtj6@gmail.com>
2025-06-22 20:41:46 +0600

    Added Problem 9

---
+++ Codes/9. Palindrome Number/Code.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        vector<int>v;
+        while(x!=0){
+            v.push_back(x%10);
+            x=x/10;
+        }
+        int i=0,j=v.size()-1;
+        while(i<=j){
+            if(v[i]!=v[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
---
+++ Codes/9. Palindrome Number/README.md
@@ -0,0 +1,3 @@
+Leetcode Problem Link: https://leetcode.com/problems/palindrome-number
+
+My solution Link On LeetCode: https://leetcode.com/problems/palindrome-number/solutions/6867782/c-beginner-friendly-easy-to-understand-b-dw7q
