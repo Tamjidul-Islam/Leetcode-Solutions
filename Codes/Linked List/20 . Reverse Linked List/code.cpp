573e22527e46e980c6546fe9a98ab679bb0472a6
Tamjidul-Islam <tamjidtj6@gmail.com>
2025-06-22 19:34:58 +0600

    Added leetcode problem 20

--- Codes/Linked List/20 . Reverse Linked List/code.cpp
+++ Codes/Linked List/20 . Reverse Linked List/code.cpp
@@ -24,5 +24,5 @@ public:
     }
 };
 
-//Leetcode Solution Link: https://leetcode.com/problems/reverse-linked-list/solutions/6869126/c-iterative-method-beats-100-by-tamjidul-teke
+Leetcode Solution Link: https://leetcode.com/problems/reverse-linked-list/solutions/6869126/c-iterative-method-beats-100-by-tamjidul-teke
 
---
+++ Guide.md
@@ -0,0 +1,4 @@
+Each Folder is Created with the Name of the Problem
+Each Folder Contains-
+1. Code
+2. A Readme which Contains Leetcode Problem Link and My Solution Link on Leetcode
